251a2c54dd1ae60ef21c216f674b1714b856798d
sumeyahussein17 <sumeyahussein21@gmail.com>
2025-03-13 20:23:29 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+//  function superbowlWin(record)=[{year:"2015", result:"w"},]
+function superbowlWin(record) {
+    let win = record.find(game => game.team === "Denver Broncos" && game.result === "W");
+    return win ? win.year : undefined;
+}
+console.log(superbowlWin(record)); 
+
+function superbowlWin(record) {
+    let win = record.find(game => game.result === "W");
+    return win ? win.year : undefined;
+}
+
